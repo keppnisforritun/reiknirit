adfeff0720242e3da40ab0cc88bfe520d2214baa
Bjarki Ágúst Guðmundsson <bjarki.agust@gmail.com>
2017-03-25 18:09:17 +0000

    Add structure for Blossom algorithm

---
+++ Blossom algorithm/tests/alternating.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <class T> int size(const T &x) { return x.size(); }
+#define rep(i,a,b) for (__typeof(a) i=(a); i<(b); ++i)
+#define iter(it,c) for (__typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef long long ll;
+const int INF = 2147483647;
+
+int main() {
+
+    // TODO: Útfæra augmenting paths reiknirit (exponential tímaflækja)
+
+    return 0;
+}
+
